8eaae767b4c231a9a241ad9b820fa7db3523ca8e
Vishesh Kumar Nayak <visheshnayak@gmail.com>
2017-07-17 11:32:51 +0530

    Create Addition.java
    Class to be tested by JUnit

---
+++ Addition.java
@@ -0,0 +1,12 @@
+package com.service;
+
+public class Addition {
+	public int add(int x, int y){return x+y;}
+	public double div(int x, int y)
+	{
+		double res = 0.0;
+		res = x/y;
+		return res; 
+	}
+
+}
